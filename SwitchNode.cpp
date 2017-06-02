ba0cd25ea48acc20790e256d2ad12029fb2eee6f
MTJoker <mail@bernd-schubart.de>
2017-06-02 19:26:16 +0200

    update SwitchNode (basic tests done)

--- SwitchNode.cpp
+++ SwitchNode.cpp
@@ -1,14 +1,13 @@
 #include "SwitchNode.hpp"
 
-const long DEBOUNCE_INTERVAL_DEFAULT = 100;
+const long DEBOUNCE_INTERVAL_DEFAULT = 50;
 
 SwitchNode::SwitchNode(const char* name, HomieNode& node, uint8_t pin)
 :
 m_name(name),
 m_node(node)
 {
-  pinMode(pin, INPUT);
-  digitalWrite(pin, HIGH);
+  pinMode(pin, INPUT_PULLUP);
 
   m_switch.attach(pin);
   m_switch.interval(DEBOUNCE_INTERVAL_DEFAULT);
@@ -16,13 +15,13 @@ m_node(node)
 
 void SwitchNode::setup()
 {
-  m_node.setProperty("state").send("open");
+  m_node.setProperty("state").send("unknown");
 }
 
 void SwitchNode::loop()
 {
   m_switch.update();
-
+  
   if(m_switch.rose())
   {
     Homie.getLogger() << m_name << ": Status is open" << endl;
